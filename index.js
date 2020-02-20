c51e66ff87976a759c13cde050c63ecaf08b504e
Kern Elder <kelel1@yahoo.com>
2020-02-20 18:02:18 -0500

    complete book constructor functionality

--- index.js
+++ index.js
@@ -5,6 +5,19 @@ function Book(title, author, pages, read) {
     this.read   = read
 }
 
+
 Book.prototype.info = function() {
     return(`${this.title} by ${this.author}, ${this.pages}, ${this.read}`)
 }
+
+function NewBook(title, author, pages, read) {
+    this.title  = title
+    this.author = author
+    this.pages  = pages
+    this.read   = read? 'has read': 'not read yet'
+}
+
+NewBook.prototype = Object.create(Book.prototype)
+
+const life = new NewBook('Life', 'Ke', 298, true)
+console.log(life.info())
